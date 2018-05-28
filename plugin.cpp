6bd7113c9c45c25c4e7bd7aa63a1682af69ce92c
Federico Ferri <federico.ferri.it@gmail.com>
2018-05-28 18:40:45 +0200

    moved Handle to v_repPlusPlus

--- plugin.cpp
+++ plugin.cpp
@@ -6,10 +6,6 @@
 #include <utility>
 #include <algorithm>
 
-#include <boost/format.hpp>
-#include <boost/regex.hpp>
-#include <boost/algorithm/string/predicate.hpp>
-
 #include <boost/config.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -18,6 +14,7 @@
 #include <boost/graph/graph_utility.hpp>
 
 #include "v_repPlusPlus/Plugin.h"
+#include "v_repPlusPlus/Handle.h"
 #include "plugin.h"
 #include "stubs.h"
 
@@ -27,48 +24,7 @@
 
 #define PROXIMITY_SENSOR_INFLATE 0.001
 
-// handle: a tool for pointer <--> string conversion
-template<typename T>
-struct Handle
-{
-    static std::string str(const T *t)
-    {
-        static boost::format fmt("%s:%lld:%d");
-        return (fmt % tag() % reinterpret_cast<long long int>(t) % crc_ptr(t)).str();
-    }
-
-    static T * obj(std::string h)
-    {
-        boost::cmatch m;
-        static boost::regex re("([^:]+):([^:]+):([^:]+)");
-        if(boost::regex_match(h.c_str(), m, re) && m[1] == tag())
-        {
-            T *t = reinterpret_cast<T*>(boost::lexical_cast<long long int>(m[2]));
-            int crc = boost::lexical_cast<int>(m[3]);
-            if(crc == crc_ptr(t)) return t;
-        }
-        return nullptr;
-    }
-
-private:
-    static std::string tag()
-    {
-        return "ptr";
-    }
-
-    static int crc_ptr(const T *t)
-    {
-        auto x = reinterpret_cast<long long int>(t);
-        x = x ^ (x >> 32);
-        x = x ^ (x >> 16);
-        x = x ^ (x >> 8);
-        x = x ^ (x >> 4);
-        x = x & 0x000000000000000F;
-        x = x ^ 0x0000000000000008;
-        return int(x);
-    }
-};
-
+using vrep::Handle;
 using OcTree = octomap::ColorOcTree;
 using OcTreeKey = octomap::OcTreeKey;
 using OcTreeNode = octomap::ColorOcTreeNode;
