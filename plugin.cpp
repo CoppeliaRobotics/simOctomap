0f67f32f51c521fce5a15e12e463f2a5e3425626
Federico Ferri <federico.ferri.it@gmail.com>
2018-07-27 23:28:40 +0200

    include git version in build date string

--- plugin.cpp
+++ plugin.cpp
@@ -1139,7 +1139,7 @@ public:
             throw std::runtime_error("failed to register script stuff");
 
         simSetModuleInfo(PLUGIN_NAME, 0, "Octomap Plugin", 0);
-        simSetModuleInfo(PLUGIN_NAME, 1, __DATE__, 0);
+        simSetModuleInfo(PLUGIN_NAME, 1, BUILD_DATE, 0);
     }
 };
 
